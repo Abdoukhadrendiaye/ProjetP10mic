eeac1e372af04cc1862f047c9a87e4cdaa3040e4
Abdoukhadrendiaye <djilyndiayekhadre@gmail.com>
2024-12-18 11:54:15 +0000

    premier depots

--- footer.php
+++ footer.php
@@ -2,6 +2,14 @@
 
 ?>
 
-<div class="bg-success fixed-bottom p-6 text-light" >
-    copyrigth 2024 prod by  youssouph
+<div class="bg-success d-flex fixed-bottom p-6 text-light" style="justify-content: space-between; margin-right: 10px;"  >
+<img src="faceboock.png" style="width: 50px;" alt="">
+<img src="youtube.png"  style="width: 50px;"alt="">
+    <p>
+        contact@unchk.edu.sn <br>
+        +221 30 180 41 53
+</p>
+
+<img src="instagramme.jpeg" style="width: 50px;" alt="">
+<img src="linkedin.png"  style="width: 50px;"alt="">
 </div>
